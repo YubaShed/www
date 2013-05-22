f951e36eb36323b5d487de896d019d5ae5370896
Max Norton <maxbnorton@gmail.com>
2013-05-22 00:52:38 -0700

    fix caps

--- sites/default/themes/yubatheme/templates/browser-overlay.php
+++ sites/default/themes/yubatheme/templates/browser-overlay.php
@@ -1,4 +1,4 @@
-<?php echo $_COOKIE["DISMISS"]; if (!isset ($_COOKIE["Dismiss"])): ?>
+<?php echo $_COOKIE["Dismiss"]; if (!isset ($_COOKIE["Dismiss"])): ?>
 
 	<div id="browser-alert-wrapper" style="position: fixed;
 	_position:absolute;
